47f0180d65433d16ed1547dde44468d523a1fa6b
DipaS3 <141767463+DipaS3@users.noreply.github.com>
2025-03-06 16:07:44 +0530

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { userRouter } from './Routes/userRoutes.js';
+import { restRouter } from './Routes/restRoutes.js';
+import mongoose from 'mongoose';
+import { v2 as cloudinary } from 'cloudinary'
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: "https://order-up-c8pt.vercel.app",  
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,   
+}));
+app.use(express.json());
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+  })
+  
+app.get("/", (req, res) =>{
+    res.json({msg : "Server is working"});
+})
+
+
+mongoose.connect(`${process.env.DATABASE_URL}`)
+ .then(()=> console.log("Connected to Database")).catch((e)=> console.log(e.code + `${" "} Error in connecting to Database`));
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/restaurant", restRouter);
+
+
+app.listen(3000);
